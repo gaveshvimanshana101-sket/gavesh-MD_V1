8823859616c556fdd5f845763df113882bc9826c
gaveshvimanshana101-sket <gaveshvimanshana101@gmail.com>
2026-03-28 19:09:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RBsTHArL#NHCQT5yzQw6gFeWSJvUKncwqryqDfptWcLZ9DajMFXA",
+SESSION_ID: process.env.SESSION_ID || "ZcUX1ajA#nlil4nLlEaMQdkJG1JaEIG1AqZZVbZRH7WeC_O7KHVQ",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/gaveshvimanshana101-sket/VIMA-MD/refs/heads/main/Image/file_00000000d22c71faade3c1e62f1313ae.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VIMA-MD Is Alive Now😍*",
 BOT_OWNER: '94789706579',  // Replace with the owner's phone number
